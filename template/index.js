78db491e36451fd7b9dc9340c52f64541ac8e8ed
Matin Kaboli <matinkaboli@aol.com>
2019-04-13 16:17:17 +0430

    feat: move http to https

--- template/index.js
+++ template/index.js
@@ -3,6 +3,14 @@ const { join } = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+
 app.use('/static', express.static(join(__dirname, '/static')));
 
 app.use((req, res) => {
